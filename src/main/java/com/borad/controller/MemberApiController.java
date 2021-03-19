307c14c24f88b67d25b64954dbfabc25446ba504
rlawls1991 <rlawls1991@gmail.com>
2021-03-19 17:44:56 +0900

    refactor - error 처리로 인한 수정

--- src/main/java/com/borad/controller/MemberApiController.java
+++ src/main/java/com/borad/controller/MemberApiController.java
@@ -5,6 +5,7 @@ import com.borad.domain.member.dto.MemberDto;
 import com.borad.domain.member.dto.MemberParamDto;
 import com.borad.domain.member.dto.MemberSearchDto;
 import com.borad.domain.member.service.MemberService;
+import com.borad.error.ErrorsResource;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,6 +15,7 @@ import org.springframework.data.web.PagedResourcesAssembler;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -29,18 +31,13 @@ public class MemberApiController {
     /**
      * 회원 가입
      * @param memberParamDto
-     * @param bindingResult
+     * @param errors
      * @return
      */
     @PostMapping
-    public ResponseEntity createMember(@Valid @RequestBody MemberParamDto memberParamDto, BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) {
-            bindingResult.getAllErrors().forEach(objectError -> {
-                log.info("code : " + objectError.getCode());
-                log.info("defaultMessage : " + objectError.getDefaultMessage());
-                log.info("objectName : " + objectError.getObjectName());
-            });
-            return ResponseEntity.badRequest().body(bindingResult);
+    public ResponseEntity<?> createMember(@Valid @RequestBody MemberParamDto memberParamDto, Errors errors) {
+        if (errors.hasErrors()) {
+            return badRequest(errors);
         }
 
         MemberDto saveMember = memberService.createMember(memberParamDto);
@@ -55,7 +52,7 @@ public class MemberApiController {
      * @return
      */
     @GetMapping("/{memberId}")
-    public ResponseEntity getMember(@PathVariable Long memberId) {
+    public ResponseEntity<?> getMember(@PathVariable Long memberId) {
         MemberDto member = memberService.findByMember(memberId);
 
         if (member == null) {
@@ -70,11 +67,10 @@ public class MemberApiController {
      *
      * @param searchDto
      * @param pageable
-     * @param assembler
      * @return
      */
     @GetMapping
-    public ResponseEntity queryMembers(MemberSearchDto searchDto, Pageable pageable) {
+    public ResponseEntity<?> queryMembers(MemberSearchDto searchDto, Pageable pageable) {
 
         MemberPages memberPages = new MemberPages(memberService.findAll(searchDto, pageable));
 
@@ -86,19 +82,14 @@ public class MemberApiController {
      *
      * @param memberId
      * @param memberParamDto
-     * @param bindingResult
+     * @param errors
      * @return
      */
     @PutMapping("/{memberId}")
-    public ResponseEntity updateMember(@PathVariable Long memberId,
-                                       @RequestBody @Valid MemberParamDto memberParamDto, BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) {
-            bindingResult.getAllErrors().forEach(objectError -> {
-                log.info("code : " + objectError.getCode());
-                log.info("defaultMessage : " + objectError.getDefaultMessage());
-                log.info("objectName : " + objectError.getObjectName());
-            });
-            return ResponseEntity.badRequest().body(bindingResult);
+    public ResponseEntity<?> updateMember(@PathVariable Long memberId,
+                                       @RequestBody @Valid MemberParamDto memberParamDto, Errors errors) {
+        if (errors.hasErrors()) {
+            return badRequest(errors);
         }
 
         MemberDto member = memberService.findByMember(memberId);
@@ -118,7 +109,7 @@ public class MemberApiController {
      * @return
      */
     @DeleteMapping("/{memberId}")
-    public ResponseEntity<MemberDto> deleteMember(@PathVariable Long memberId) {
+    public ResponseEntity<?> deleteMember(@PathVariable Long memberId) {
         MemberDto member = memberService.findByMember(memberId);
         if (member == null) {
             return ResponseEntity.notFound().build();
@@ -136,6 +127,10 @@ public class MemberApiController {
         public MemberPages(Page<MemberDto> memberPages) {
             this.page = memberPages;
         }
+    }
+
+    private ResponseEntity<?> badRequest(Errors errors) {
+        return ResponseEntity.badRequest().body(new ErrorsResource(errors));
     }
 
 }
